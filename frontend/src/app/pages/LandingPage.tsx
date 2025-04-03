30d1a75eb0efd9bff18a609b12b4367077b14f64
khayweee <pineapplekw-dev@pineapplekw-pc>
2025-04-04 00:20:53 +0800

    feat: Landing page game room not found modal

--- frontend/src/app/pages/LandingPage.tsx
+++ frontend/src/app/pages/LandingPage.tsx
@@ -2,13 +2,23 @@
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { saveToken } from "../utils/auth";
-import { Button, Input, Form, Flex, Typography, Card, Space } from "antd";
+import {
+  Button,
+  Input,
+  Form,
+  Flex,
+  Typography,
+  Card,
+  Space,
+  Modal,
+} from "antd";
 
 const { Title } = Typography;
 
 const LandingPage = () => {
   const [name, setName] = useState("");
   const [sessionId, setSessionId] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false); // Game room not found modal
   const router = useRouter();
 
   const handleCreateGame = async () => {
@@ -36,68 +46,88 @@ const LandingPage = () => {
   };
 
   const handleJoinGame = async () => {
-    const createPlayerResponse = await fetch(
-      "https://localhost:8000/game/add-player",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ session_id: sessionId, name: name }),
+    try {
+      const createPlayerResponse = await fetch(
+        "https://localhost:8000/game/add-player",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ session_id: sessionId, name: name }),
+        }
+      );
+      if (!createPlayerResponse.ok) {
+        throw new Error("Game session not found");
       }
-    );
-    const createPlayerData = await createPlayerResponse.json();
-    saveToken(createPlayerData.token); // Save the token to local storage
-    console.log(createPlayerData);
+      const createPlayerData = await createPlayerResponse.json();
 
-    // Pass the token to the next page
-    router.push(`/game/${sessionId}/${createPlayerData.player.id}`);
+      // Save the token to local storage
+      saveToken(createPlayerData.token);
+
+      // Pass the token to the next page
+      router.push(`/game/${sessionId}/${createPlayerData.player.id}`);
+    } catch (error) {
+      console.error(error);
+      setIsModalVisible(true);
+    }
   };
 
   return (
-    <Flex
-      className="landing-page"
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <Card style={{ width: 400, textAlign: "center" }} bordered={true}>
-        <Title level={2}>Mr. Lim Card Game</Title>
-        <Form name="start-game" style={{ maxWidth: 360, margin: "0 auto" }}>
-          <Form.Item
-            name="name"
-            rules={[{ required: true, message: "Please input your name!" }]}
-          >
-            <Input
-              placeholder="Player Name"
-              size="large"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </Form.Item>
-          <Form.Item name="game-session">
-            <Space.Compact style={{ width: "100%" }}>
+    <>
+      <Flex
+        className="landing-page"
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <Card style={{ width: 400, textAlign: "center" }} bordered={true}>
+          <Title level={2}>Mr. Lim Card Game</Title>
+          <Form name="start-game" style={{ maxWidth: 360, margin: "0 auto" }}>
+            <Form.Item
+              name="name"
+              rules={[{ required: true, message: "Please input your name!" }]}
+            >
               <Input
-                placeholder="Game Room"
+                placeholder="Player Name"
                 size="large"
-                value={sessionId}
-                onChange={(e) => setSessionId(e.target.value)}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
-              <Button size="large" type="primary" onClick={handleJoinGame}>
-                Join
-              </Button>
-            </Space.Compact>
-          </Form.Item>
-          <Form.Item>
-            <Flex justify="center">
-              <Button size="large" type="primary" onClick={handleCreateGame}>
-                Create Game
-              </Button>
-            </Flex>
-          </Form.Item>
-        </Form>
-      </Card>
-    </Flex>
+            </Form.Item>
+            <Form.Item name="game-session">
+              <Space.Compact style={{ width: "100%" }}>
+                <Input
+                  placeholder="Game Room"
+                  size="large"
+                  value={sessionId}
+                  onChange={(e) => setSessionId(e.target.value)}
+                />
+                <Button size="large" type="primary" onClick={handleJoinGame}>
+                  Join
+                </Button>
+              </Space.Compact>
+            </Form.Item>
+            <Form.Item>
+              <Flex justify="center">
+                <Button size="large" type="primary" onClick={handleCreateGame}>
+                  Create Game
+                </Button>
+              </Flex>
+            </Form.Item>
+          </Form>
+        </Card>
+      </Flex>
+      <Modal
+        title="Game Room Not Found"
+        open={isModalVisible}
+        onOk={() => setIsModalVisible(false)}
+        onCancel={() => setIsModalVisible(false)}
+        okText="OK"
+      >
+        <p>Game room not found. Please check the Game Room and try again.</p>
+      </Modal>
+    </>
   );
 };
 
